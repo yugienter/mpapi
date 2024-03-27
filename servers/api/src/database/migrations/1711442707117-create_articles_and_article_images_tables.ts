285cda6df5679bfece8d7a8d20023355d88c58dd
Yugienter Nguyen <yugienter@MacBook-Pro-of-Yugienter.local>
2024-03-27 15:25:15 +0700

    Create migration for articles and article images tables

---
+++ servers/api/src/database/migrations/1711442707117-create_articles_and_article_images_tables.ts
@@ -0,0 +1,136 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
+
+import { ArticleStatus } from '@/app/models/articles';
+
+import { TIME_STAMPS_WITH_SOFT_DELETE } from './common/migration-util';
+
+export class CreateArticlesAndArticleImagesTable1711442707117 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'articles',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'title',
+            type: 'text',
+          },
+          {
+            name: 'content',
+            type: 'text',
+          },
+          {
+            name: 'status',
+            type: 'enum',
+            enum: Object.values(ArticleStatus),
+            default: `'${ArticleStatus.DRAFT}'`,
+          },
+          {
+            name: 'author_id',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FK_author_article',
+            columnNames: ['author_id'],
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            onDelete: 'NO ACTION',
+          },
+        ],
+      }),
+      true,
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'article_images',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '500',
+            isNullable: false,
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'size',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'path',
+            type: 'text',
+            isNullable: false,
+          },
+          {
+            name: 'is_deleted',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'article_id',
+            type: 'int',
+          },
+          {
+            name: 'created_by',
+            type: 'varchar',
+            isNullable: false,
+          },
+          ...TIME_STAMPS_WITH_SOFT_DELETE,
+        ],
+        foreignKeys: [
+          new TableForeignKey({
+            name: 'FK_article_images_users',
+            columnNames: ['created_by'],
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            onDelete: 'NO ACTION',
+          }),
+          new TableForeignKey({
+            name: 'FK_article_images_articles',
+            columnNames: ['article_id'],
+            referencedTableName: 'articles',
+            referencedColumnNames: ['id'],
+            onDelete: 'NO ACTION',
+          }),
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('article_images');
+    await queryRunner.dropTable('articles');
+  }
+}
